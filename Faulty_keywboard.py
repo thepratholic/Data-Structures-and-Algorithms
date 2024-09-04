2450efff1293e28db37bf2c51c946826fddb5b69
chelaramanipratham <100186226+chelaramanipratham@users.noreply.github.com>
2024-09-04 09:45:58 +0530

    Update Faulty_keywboard.py

--- Faulty_keywboard.py
+++ Faulty_keywboard.py
@@ -4,8 +4,8 @@ class Solution:
         revstr = ""
         for c in s:
             if c != "i":
-                strr += c
-                revstr += c
+                strr = strr + c
+                revstr = c + revstr
             else:
                 strr, revstr = revstr, strr
         return strr
