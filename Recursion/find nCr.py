68260ed325eaae99f2c0d67e605740ca0c1bb889
chelaramanipratham <chelaramanipratham@gmail.com>
2024-10-03 23:34:03 +0530

    Add files via upload

---
+++ Recursion/find nCr.py
@@ -0,0 +1,9 @@
+def fact(n):
+    if n==0: return 1
+    else: return fact(n-1) * n
+
+
+def nCr(n,r):
+    #code here
+    if r > n: return 0
+    return fact(n) // (fact(r) * fact(n-r))
--- Recursion/josephus problem.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def josephus(self,n,k):
-        if(n ==1):
-            return n
-
-        return (self.josephus(n-1,k)+k-1)%n+1
-        #Your code here
