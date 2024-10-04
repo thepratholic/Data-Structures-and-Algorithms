676473f68c0a649d3283da13b90ab03253450498
chelaramanipratham <chelaramanipratham@gmail.com>
2024-10-04 13:38:46 +0530

    Add files via upload

--- Searching/binary_search.py
+++ Searching/binary_search.py
@@ -1,29 +1,17 @@
 def binarySearch(arr, l, r, x):
 
-    # Check base case
-    if r >= l: return
+    if l >= r: return
 
-    mid = l + (r - l) // 2
+    low, high = 0, len(arr)-1
+    while low <= high:
+        mid = (low + high) // 2
 
-    # If element is present at the middle itself
-    if arr[mid] == x:
-        return mid
-
-    # If element is smaller than mid, then it
-    # can only be present in left subarray
-    elif arr[mid] > x:
-        return binarySearch(arr, l, mid-1, x)
-
-    # Else the element can only be present
-    # in right subarray
-    elif(arr[mid]  < x):
-        return binarySearch(arr, mid + 1, r, x)
-
-    else:
-        # Element is not present in the array
-        return -1
+        if arr[mid] == x: return mid
+        elif arr[mid] < x: low = mid + 1
+        else: high = mid - 1
+    return -1
 
 
-# Driver Code
+
 arr = [2, 3, 4, 10, 40]
 x = 10
