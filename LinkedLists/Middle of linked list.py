a9cd737766c79c1892c968cf5706778cc70501ea
chelaramanipratham <chelaramanipratham@gmail.com>
2024-10-10 20:54:35 +0530

    Add files via upload

--- LinkedLists/Middle of linked list.py
+++ LinkedLists/Middle of linked list.py
@@ -20,6 +20,7 @@ def middle(head):
 
 head = Node(10)
 head.next = Node(20)
-head.next.next = Node(30)
-# printList(head)
+head.next.next = Node(40)
+# head.next.next.next = Node(40)
+printList(head)
 print(middle(head))
