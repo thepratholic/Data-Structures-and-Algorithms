0bbacfb72c2891335f47181ae6156f753fb91225
chelaramanipratham <chelaramanipratham@gmail.com>
2024-10-10 15:49:58 +0530

    Add files via upload

--- LinkedLists/insertAtPosition.py
+++ LinkedLists/insertAtPosition.py
@@ -6,14 +6,18 @@ class Node:
 def InsertAtPosition(head, pos, x):
     temp = Node(x)
     if head is None: return temp
+    cnt = 0
     curr = head
-    for i in range(pos - 2):
-        if curr is None: return head
+    while curr != None:
+        cnt += 1
+        curr = curr.next
+    if pos > cnt: return None
+    curr = head
+    for i in range(pos-1):
         curr = curr.next
     temp.next = curr.next
     curr.next = temp
     return head
-
 
 def printList(head):
     temp = head
@@ -27,5 +31,5 @@ head.next = Node(20)
 head.next.next = Node(30)
 head.next.next.next = Node(40)
 head.next.next.next.next = Node(50)
-head = InsertAtPosition(head, 5, 45)
+head = InsertAtPosition(head, 4, 45)
 printList(head)
