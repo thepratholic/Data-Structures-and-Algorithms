9f6172c919d8ed5110d64cbc1163ecc117c0725a
chelaramanipratham <chelaramanipratham@gmail.com>
2024-10-12 19:53:41 +0530

    Updated

--- Queue/Implementation using arrays.py
+++ Queue/Implementation using arrays.py
@@ -1,7 +1,8 @@
 class Queue:
     def __init__(self, c):
         self.queue = []
-        self.rear = self.front = 0
+        self.rear = 0
+        self.front = -1
         self.qsize = 0
         self.capacity = c
 
@@ -12,9 +13,10 @@ class Queue:
 
     def dequeue(self):
         if self.front == self.rear: print("Queue is empty\n")
-        d = self.queue.pop(0)
-        self.front += 1
-        return d
+        else:
+            d = self.queue.pop(0)
+            self.front += 1
+            return d
 
     def queueDisplay(self):
         if self.front == self.rear: print("Queue is empty\n")
@@ -23,11 +25,13 @@ class Queue:
 
     def getFront(self):
         if self.front == self.rear: print("Queue is empty\n")
-        return self.queue[self.front]
+        return self.queue[0]
+    
     
 q = Queue(4)
 q.enqueue(10)
 q.enqueue(20)
 q.enqueue(30)
 print(q.dequeue())
-q.queueDisplay()
+# q.queueDisplay()
+print(q.getFront())
