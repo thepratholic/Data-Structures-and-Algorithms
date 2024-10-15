b78ddcc4533daa6c75e99d416f7ebb5146a82b86
chelaramanipratham <chelaramanipratham@gmail.com>
2024-10-15 13:04:33 +0530

    Add files via upload: cd .. git add .

---
+++ Trees/Height of the Tree.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def Height(root):
+    if root is None:
+        return 0
+    else:
+        lh = Height(root.left)
+        rh = Height(root.right)
+
+        return max(lh, rh) + 1
+
+root = Node(10)
+root.left = Node(20)
+root.right = Node(30)
+root.left.left = Node(100)
+print(Height(root))
