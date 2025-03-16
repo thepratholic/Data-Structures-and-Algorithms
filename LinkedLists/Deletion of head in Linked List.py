10deb8b66ea956ca48831cfac5b3991584912ad4
thepratholic <chelaramanipratham@gmail.com>
2025-03-16 09:53:10 +0530

    Linked lists

---
+++ LinkedLists/Deletion of head in Linked List.py
@@ -0,0 +1,11 @@
+# Definiton of singly Linked List
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def deleteHead(self, head):
+        temp = head
+        head = head.next
+        return head
