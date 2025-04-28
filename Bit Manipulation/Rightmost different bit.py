5d4d30dff6f250d20e9224a07a4183e732868916
thepratholic <chelaramanipratham@gmail.com>
2025-04-28 14:41:54 +0530

    Bit Manipulation

--- Bit Manipulation/Rightmost different bit.py
+++
@@ -1,9 +0,0 @@
-def posOfRightMostDiffBit(self,m,n):
-
-        ans = m ^ n
-        if ans == 0: return -1
-        pos = 0
-        while ans & 1 == 0:
-            ans >>= 1
-            pos += 1
-        return pos + 1
