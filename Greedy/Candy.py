cdcf4ec62214cbc9bbbfd1af15a5e355fe5cae74
thepratholic <chelaramanipratham@gmail.com>
2025-05-17 23:15:43 +0530

    Greedy

---
+++ Greedy/Candy.py
@@ -0,0 +1,30 @@
+class Solution:
+    def candy(self, ratings):
+        #your code goes here
+        n = len(ratings)
+
+        sum = 1
+        i = 1
+
+        while i < n:
+            if ratings[i] == ratings[i-1]:
+                sum += 1
+                i += 1
+                continue
+
+            peak = 1
+            while i < n and ratings[i] > ratings[i-1]:
+                peak += 1
+                sum += peak
+                i += 1
+
+            down = 1
+            while i < n and ratings[i] < ratings[i-1]:
+                sum += down
+                down += 1
+                i += 1
+
+            if down > peak:
+                sum += (down - peak)
+
+        return sum
