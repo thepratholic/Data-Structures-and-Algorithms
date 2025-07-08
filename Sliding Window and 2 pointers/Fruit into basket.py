ff50ae52c02835ff26ee1aa95200374ead87b028
thepratholic <chelaramanipratham@gmail.com>
2025-07-08 15:25:41 +0530

    Sliding window and 2 pointers

--- Sliding Window and 2 pointers/Fruit into basket.py
+++ Sliding Window and 2 pointers/Fruit into basket.py
@@ -1,23 +1,24 @@
+from collections import defaultdict
 class Solution:
-    def totalFruit(self, fruits):
-        n, l, r, maxLen, mpp = len(fruits), 0, 0, 0, {}
+    def totalFruits(self, fruits):
+        #your code goes here
+        n = len(fruits)
+        l, r = 0, 0
+        max_len = 0
+        mpp = defaultdict(int)
 
         while r < n:
-            if fruits[r] in mpp:
-                mpp[fruits[r]] += 1
-            else:
-                mpp[fruits[r]] = 1
+            mpp[fruits[r]] += 1
 
             if len(mpp) > 2:
                 mpp[fruits[l]] -= 1
                 if mpp[fruits[l]] == 0:
                     del mpp[fruits[l]]
+
                 l += 1
 
             if len(mpp) <= 2:
-                maxLen = max(maxLen, r - l + 1)
+                max_len = max(max_len, r - l + 1)
             r += 1
-        return maxLen
 
-s = Solution()
-print(s.totalFruit([1,2,3,2,2]))
+        return max_len
