1bc9a2ade37cd036623d93f08df09b71309c9bd4
thepratholic <chelaramanipratham@gmail.com>
2025-08-28 20:18:27 +0530

    MCM DP

--- Dynamic Programming/Partition_DP_Aditya_Verma/Palindrome_Partitioning.py
+++ Dynamic Programming/Partition_DP_Aditya_Verma/Palindrome_Partitioning.py
@@ -7,24 +7,30 @@ def isPalin(s, i, j):
 
     return True
 
-def solve(s, i, j):
+def solve(s, i, j, dp):
     if i >= j:
         return 0
     
     if isPalin(s, i, j):
         return 0
     
+    if dp[i][j] != -1:
+        return dp[i][j]
+    
     mini = float('inf')
     for k in range(i, j):
-        temp = 1 + solve(s, i, k) + solve(s, k + 1, j)
+        temp = 1 + solve(s, i, k, dp) + solve(s, k + 1, j, dp)
         mini = min(mini, temp)
 
-    return mini
+    dp[i][j] = mini
+    return dp[i][j]
 
 
 def main():
     s = "nitin"
-    print(solve(s, 0, 4))
+    n = len(s)
+    dp = [[-1] * n for _ in range(n)]
+    print(solve(s, 0, n - 1, dp))
 
 if __name__ == "__main__":
     main()
