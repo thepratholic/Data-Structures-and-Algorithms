5434b3398fc81f1bf89c82f2a9ed5d9e3b202a0e
thepratholic <chelaramanipratham@gmail.com>
2025-10-09 17:05:33 +0530

    Binary Trees

---
+++ Trees/FAQs/Bottom view of BT.py
@@ -0,0 +1,33 @@
+from collections import deque, defaultdict
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.data = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def bottomView(self, root):
+        if not root:
+            return []
+
+        q = deque([(root, 0)]) # node, column
+        column_map = defaultdict(list)
+
+        ans = []
+
+        while q:
+            for _ in range(len(q)):
+                node, column = q.popleft()
+                column_map[column].append(node.data)
+
+                if node.left:
+                    q.append((node.left, column - 1))
+
+                if node.right:
+                    q.append((node.right, column + 1))
+
+        for col in sorted(column_map.keys()):
+            ans.append(column_map[col][-1])
+
+        return ans
