add2bfeb30f82d5ea72ac5bd3dfdb9eb0d6a02b5
thepratholic <chelaramanipratham@gmail.com>
2025-10-09 17:07:30 +0530

    Binary Trees

---
+++ Trees/FAQs/LCA in BT.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.data = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        if not root or root == p or root == q:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left is None:
+            return right
+
+        elif right is None:
+            return left
+
+        else:
+            return root
